80c9390e96adf590761edd58fa1811fa94c2f305
Awel Eshetu <aweloe@gmail.com>
2023-09-08 13:57:32 +0300

    Chapter 2
     - contains exercieses 2.11

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Contacts from './components/Contacts'
 import Filter from './components/Filter'
 import ContactForm from './components/ContactForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchText, setSearchText] = useState('')
 
-  
+  useEffect(() => { 
+    axios.get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data))
+  }, [])
+
   const handleNameChange = (event) => { setNewName(event.target.value) }
   const handleNumberChange = (event) => { setNewNumber(event.target.value) }
   const handleSearchText = (event) => { 
